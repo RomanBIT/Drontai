b13e5b197a1343a8f7d8cc75fa12b941d6228d0e
Roman-BIT <laskov.roman@gmail.com>
2022-08-26 20:30:16 +0300

    null

--- r1/src/Components/003/Dog.jsx
+++ r1/src/Components/003/Dog.jsx
@@ -1,9 +1,18 @@
+import rand from '../../Functions/rand';
+
 function Dog() {
+
+    if (rand(0, 1)) {
+        return null;
+    }
 
     return (
         <>
-            <h2>Dog {true ? 'Jo' : 'Ne jo'}</h2>
-            <h3>Big</h3>
+            <h2 style={{
+                color: ['crimson', 'skyblue', 'orange'][rand(0, 2)],
+                fontSize: rand(0, 1) ? '40px' : null
+            }}>Dog {true ? 'Jo' : 'Ne jo'}</h2>
+            <h3 className={rand(0, 1) ? 'cat' : 'dog'}>Big {rand(100, 999)}</h3>
         </>
     )
 }

ce9332873fc5342f5c548b73c716072f25a583fb
Roman-BIT <laskov.roman@gmail.com>
2022-08-26 20:54:08 +0300

    Racoon color

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
-import Dog from './Components/003/Dog';
+// import Dog from './Components/003/Dog';
+// import Cat from './Components/003/Cat';
+import Racoon from './Components/003/Racoon';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Dog></Dog>
+        {/* <Dog></Dog> */}
         <h1>Start</h1>
-        <Dog></Dog>
+        {/* <Dog></Dog> */}
+        {/* <Cat /> */}
+        <Racoon />
       </header>
     </div>
   );

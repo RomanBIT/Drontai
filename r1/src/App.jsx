bca744072d1f50376792e79ce932fe6419838add
Roman-BIT <laskov.roman@gmail.com>
2022-08-29 21:04:17 +0300

    005 39:30

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -2,13 +2,26 @@ import './App.scss';
 
 
 function App() {
+
+    const clicked = function () {
+        console.log('Labas');
+
+    }
+    const doJob = function () {
+        console.log('Dirbam');
+        clicked();
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-             <h1>STATE</h1>  
+                <h1>STATE</h1>
+                <button onClick={doJob}>ClICK!</button>
             </header>
         </div>
     );
 }
+
+//---------------005 39:30---------------//
 
 export default App;

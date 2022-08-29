3f77f223de7d6af912c29a82874b1fb6803c97b2
Roman-BIT <laskov.roman@gmail.com>
2022-08-29 18:48:18 +0300

    sort ABC cats

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,21 +1,33 @@
 import './App.css';
-// import Dog from './Components/003/Dog';
-// import Cat from './Components/003/Cat';
-import Racoon from './Components/003/Racoon';
 
+const cats = [
+    { id: 1, weight: 5, name: 'Murka' },
+    { id: 2, weight: 8, name: 'Pilkis' },
+    { id: 3, weight: 2, name: 'Pūkis' },
+    { id: 4, weight: 3, name: 'Rainis' },
+    { id: 5, weight: 5, name: 'Brisius' }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Dog></Dog> */}
-        <h1>Start</h1>
-        {/* <Dog></Dog> */}
-        {/* <Cat /> */}
-        <Racoon />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Lists</h1>
+                <ul>
+                    {
+                        [...cats].sort((a, b) => b.weight - a.weight).map((cat, i) =>
+                            <li key={i}>{cat.name} <i>{cat.weight}kg</i></li>)
+                    }
+                </ul>
+                <ul>
+                    {
+                        [...cats].sort((a, b) => a.name.localeCompare(b.name)).map((cat, i) =>
+                            <li key={i}>{cat.name} <i>{cat.weight}kg</i></li>)
+                    }
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;

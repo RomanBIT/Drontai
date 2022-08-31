6a99629d1e1c12f2b338614d38654f25a44d0eb7
Roman-BIT <laskov.roman@gmail.com>
2022-08-31 20:54:51 +0300

    circle

--- r1/src/Components/006/Square.jsx
+++ r1/src/Components/006/Square.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
+import rand from '../../Functions/rand';
+import randColor from '../../Functions/randColor';
+
+
 
 function Squares() {
 
     const [sq, setSq] = useState([])
 
     const add = () => {
-        setSq(s => [...s, ''])
+        setSq(s => [...s, { number: rand(100, 999), color: randColor() }])
     }
 
     return (
         <>
             <div className="container">
+
                 {
-                    sq.map((_, i) => <div key={i}></div>)
+                    sq.map((n, i) => <div style={{ 
+                        backgroundColor: n.color,
+                        borderRadius: n.number % 2 ? null : '50%'
+                     }} key={i}>{n.number}</div>)
                 }
             </div>
             <button onClick={add}>add []</button>

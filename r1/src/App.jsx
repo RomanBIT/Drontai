38d9a481bbe94ce9a24b10b53072cbf6784595e9
Roman-BIT <laskov.roman@gmail.com>
2022-08-31 18:55:34 +0300

    tru state

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,27 +1,49 @@
+import { useState } from 'react';
 import './App.scss';
 
 
 function App() {
 
-    const clicked = function () {
-        console.log('Labas');
+    const [color, setColor] = useState('crimson');
+    const [fontSize, setFontSize] = useState(null);
+    const [count, setCount] = useState(1);
 
+    const clicked = () => {
+        setColor('skyblue')
     }
-    const doJob = function () {
-        console.log('Dirbam');
-        clicked();
+    const clickFont = () => {
+        setFontSize('100')
     }
+    // const doCount = () => {
+    //     setCount(count + 1)
+    // }
+ 
+    const doCount = () => {
+        setCount(c => c + 1)
+        setCount(c => c + 1)
+        setCount(c => c + 1)
+    }
+ 
+
+    // const clicked = () => {
+    //     console.log('Labas');
+    // }
+    // const doJob = () => {
+    //     console.log('Dirbam');
+    //     clicked();
+    // }
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>STATE</h1>
-                <button onClick={doJob}>ClICK!</button>
+                <h1 style={{ color: color, fontSize: fontSize + 'px' }}>STATE {count}</h1>
+                <button onClick={clicked}>ClICK!</button>
+                <button onClick={clickFont}>DO SIZE!</button>
+                <button onClick={doCount}>SKAICIUOJAM! +1</button>
             </header>
         </div>
     );
 }
 
-//---------------005 39:30---------------//
 
 export default App;

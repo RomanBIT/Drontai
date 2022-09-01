06404995d3693f4855e8bb4c457c78d20aef0ebb
Roman-BIT <laskov.roman@gmail.com>
2022-09-01 18:40:05 +0300

    show blck clr

--- r1/src/Components/006/Square.jsx
+++ r1/src/Components/006/Square.jsx
@@ -9,7 +9,12 @@ function Squares() {
     const [sq, setSq] = useState([])
 
     const add = () => {
-        setSq(s => [...s, { number: rand(100, 999), color: randColor() }])
+        setSq(s => [...s, {
+            number: rand(100, 999),
+            color: randColor(),
+            row: s.length,
+            show: true
+        }])
     }
 
     const sortUp = () => {
@@ -19,30 +24,45 @@ function Squares() {
         setSq(s => [...s].sort((a, b) => a.number - b.number))
     }
 
-    return (
-        <>
-            <h1>STATE {sq.filter(s => s.number < 300).length}</h1>
-            <div className="container">
+    const sortDefault = () => {
+        setSq(s => [...s].sort((a, b) => a.row - b.row))
+    }
 
-                {
-                    sq.map((n, i) => <div style={{
-                        backgroundColor: n.number < 300 ? 'black' : n.color,
-                        borderRadius: n.number % 2 ? null : '50%'
-                    }} key={i}>{n.number}</div>)
+        const filterBlack = () => {
+            setSq(s => s.map(square => square.number < 300 ? { ...square, show: true } : { ...square, show: false }))
+    }
+
+        const filterColor = () => {
+            setSq(s => s.map(square => square.number < 300 ? { ...square, show: false } : { ...square, show: true }))
+    }
+
+
+        return (
+            <>
+                <h1>STATE {sq.filter(s => s.number < 300).length}</h1>
+                <div className="container">
+
+                    {
+                        sq.map((n, i) => n.show ? <div style={{
+                            backgroundColor: n.number < 300 ? 'black' : n.color,
+                            borderRadius: n.number % 2 ? null : '50%'
+                        }} key={i}>{n.number}</div> : null)
+                    }
+                </div>
+                <div className="container">
+                    <button onClick={add}>add []</button>
+                    <button onClick={sortUp}>SORT 9-0</button>
+                    <button onClick={sortDown}>SORT 0-9</button>
+                    <button onClick={sortDefault}>SORT DEFAULT</button>
+                    <button onClick={filterBlack}>SHOW BLCK</button>
+                    <button onClick={filterColor}>SHOW CLR</button>
+                </div>
+
+            </>
+        )
+
                 }
-            </div>
-            <div className="container">
-                <button onClick={add}>add []</button>
-                <button onClick={sortUp}>SORT 9-0</button>
-                <button onClick={sortDown}>SORT 0-9</button>
-            </div>
 
-        </>
-    )
-
-}
-
-export default Squares;
+    export default Squares;
 
 
-//-------------- 007 31:57 ----------------//

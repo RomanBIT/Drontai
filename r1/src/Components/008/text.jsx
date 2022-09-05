675a43ed691df217f6e96d86e0f0ed940dd5fc1d
Roman-BIT <laskov.roman@gmail.com>
2022-09-05 20:04:52 +0300

    range

--- r1/src/Components/008/text.jsx
+++ r1/src/Components/008/text.jsx
@@ -1,20 +1,39 @@
-import {useState} from 'react';
+import { useState } from 'react';
 function Text() {
 
     const [text, setText] = useState('')
+    const [color, setColor] = useState('red')
+    const [range, setRange] = useState(0)
 
-const control = e => {
-    if (Number(e.target.value) == e.target.value) {
-        setText(e.target.value);
+    const control = e => {
+        //   Number(e.target.value) && setText(e.target.value);
+        setText(e.target.value)
+    }
+    const colorize = e => {
+        setColor(e.target.value)
+    }
+    const rangeControl = e => {
+        setRange(e.target.value)
     }
 
-    }
+
 
     return (
-        <div className="form-container">
-            <h2>TEXT</h2>
-            <input type="text" onChange={control} value={text} />
-        </div>
+        <>
+            <div className="form-container">
+                <h2>TEXT</h2>
+                <input type="text" onChange={control} value={text} />
+            </div>
+            <div className="form-container">
+                <h2 style={{ color }}>COLOR</h2>
+                <input type="color" onChange={colorize} value={color} />
+            </div>
+            <div className="form-container">
+                <h2>RANGE: { range.padStart(3, 0) }</h2>
+                <input type="range" onChange={rangeControl} value={range} />
+            </div>
+
+        </>
     )
 }
 

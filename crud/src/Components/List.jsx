e7a87eb04cc9e708cd01cf2aa73581376e357d60
Roman-BIT <laskov.roman@gmail.com>
2022-09-13 19:31:58 +0300

    CRUD done

---
+++ crud/src/Components/List.jsx
@@ -0,0 +1,24 @@
+
+import { useContext } from "react";
+import DataContext from "./DataContext";
+import Row from "./Row";
+
+function List() {
+
+    const { things } = useContext(DataContext);
+
+    return (
+        <div className="card">
+            <div className="top">
+                List of Things
+            </div>
+            <div className="body">
+                {
+                    things?.map(t => <Row key={t.id} thing={t} />)
+                }
+            </div>
+        </div>
+    )
+}
+
+export default List;

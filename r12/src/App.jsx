e853660811bf793919f19ba3daf7d24d4462c668
Roman-BIT <laskov.roman@gmail.com>
2022-09-15 18:23:32 +0300

    rand colors kv

--- r12/src/App.jsx
+++ r12/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.scss';
+import T1 from './Components/014/T1';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <div className="App">
             <header className="App-header">
               <h1>Total Recall 1</h1>
+              <T1 />
             </header>
         </div>
     );
---
+++ r12/src/Components/014/T1.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import randColor from '../../Functions/randColor'
+
+function T1() {
+
+    const [colors, setColors] = useState({
+        ba: randColor(),
+        bo: randColor()
+    })
+
+    const co = () => {
+        setColors({
+            ba: randColor(),
+            bo: randColor()
+        })
+    }
+
+    return (
+        <>
+            <div className="kv" style={{
+                backgroundColor: colors.ba,
+                border: '6px solid' + colors.bo
+            }}></div>
+            <button onClick={co}>DO</button>
+        </>
+    )
+}
+
+export default T1;

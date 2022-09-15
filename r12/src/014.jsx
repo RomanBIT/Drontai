6215cf125abdb77c5cfab69a94573b62cf1a363d
Roman-BIT <laskov.roman@gmail.com>
2022-09-15 19:46:20 +0300

    T11/T12

---
+++ r12/src/014.jsx
@@ -0,0 +1,21 @@
+
+import './App.scss';
+import T7 from './Components/014/T7';
+
+
+function App() {
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Total Recall 1</h1>
+                <T7 />
+            </header>
+        </div>
+    );
+}
+
+export default App;
+
+
--- r12/src/App.jsx
+++ r12/src/App.jsx
@@ -1,16 +1,20 @@
 
+import { useState } from 'react';
 import './App.scss';
-import T7 from './Components/014/T7';
-
+import T11 from './Components/015/T11';
+import T12 from './Components/015/T12';
 
 function App() {
+
+    const [add19, setAdd19] = useState(0);
 
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Total Recall 1</h1>
-                <T7 />
+                <h1>Total Recall 2</h1>
+                <T11 setAdd19={setAdd19}></T11>
+                <T12 add19={add19}></T12>
             </header>
         </div>
     );
